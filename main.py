f34eadda5329c21bf3c8edd41da0edebce725b24
juanj72TC <juan.jara@teamcore.cl>
2025-08-11 08:33:56 -0500

    chore: linter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.config import Config
-from src.app.router import RouterRipley
+from src.app.router.router_ripley import RouterRipley
 from fastapi import FastAPI
 from src.scraper.ripley import RipleyScraper
 
@@ -12,8 +12,3 @@ router_ripley = RouterRipley(scraper_ripley)
 app = FastAPI()
 
 app.include_router(router_ripley.get_instance())
-
-# if __name__ == "__main__":
-#     config = Config()
-#     scraper = RipleyScraper(config)
-#     print(scraper.run())
